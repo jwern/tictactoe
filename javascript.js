d233bb152382aed1d43cb7e90f5413a78342ed60
Jillian Werner <jillianlwerner@gmail.com>
2021-01-07 18:07:15 -0500

    Create function for building initial gameboard

--- javascript.js
+++ javascript.js
@@ -1,11 +1,29 @@
 // IIFE initialized immediately
 const Gameboard = (() => {
-  let rows = [
-    ["x", "o", "x"],
-    ["o", "x", "o"],
-    [" ", "o", "o"]
-  ];
-  return { rows };
+  // let rows = [
+  //   ["x", "o", "x"],
+  //   ["o", "x", "o"],
+  //   [" ", "o", "o"]
+  // ];
+  let rows = [...Array(3)].map(() => Array(3).fill("x"));
+
+  const buildGameBoard = () => {
+    let board = document.getElementById('game-board');
+    for (row of rows) {
+      let rowIndex = rows.indexOf(row);
+      row.forEach((item, index) => {
+        let square = document.createElement('div');
+        square.classList.add('board-square');
+        square.setAttribute('data-id', `${rowIndex}-${index}`);
+        square.innerText = item;
+        board.append(square);
+      })
+    }
+  }
+  return { 
+    rows, 
+    buildGameBoard 
+  };
 })()
 
 // Factory Function that can be used to create players
@@ -17,10 +35,13 @@ const Player = playerName => {
 const PlayGame = (() => {
   // let player1 = Player(prompt("Who is player 1?"));
   // let player2 = Player(prompt("And player 2?"));
-  let player1 = "Me";
-  let player2 = "You";
-  let gameStart = console.log(player1.name, player2.name);
+  let player1 = { name: "Me" };
+  let player2 = { name: "You" };
+  let gameStart = () => {
+    console.log(`The game has begun, ${player1.name} & ${player2.name}`);
+    Gameboard.buildGameBoard();
+  }
   return { gameStart };
 })()
 
-PlayGame.gameStart;
+PlayGame.gameStart();
