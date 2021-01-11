2bd8f88d809516282fabb51b028939ae07ce77ab
Jillian Werner <jillianlwerner@gmail.com>
2021-01-11 14:56:07 -0500

    Extract board & grid width and height values

--- javascript.js
+++ javascript.js
@@ -5,15 +5,25 @@ const Gameboard = (() => {
   //   ["o", "x", "o"],
   //   [" ", "o", "o"]
   // ];
-  let rows = [...Array(3)].map(() => Array(3).fill("x"));
+  let boardWidth = 3;
+  let boardHeight = 3;
+  let rows = [...Array(boardHeight)].map(() => Array(boardWidth).fill("x"));
 
   const buildGameBoard = () => {
     let board = document.getElementById('game-board');
+    board.style.gridTemplateRows = `repeat(${boardHeight}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${boardWidth}, 1fr)`;
     for (row of rows) {
       let rowIndex = rows.indexOf(row);
       row.forEach((item, index) => {
         let square = document.createElement('div');
         square.classList.add('board-square');
+        if (rowIndex !== rows.length - 1) {
+          square.classList.add('bottom-border');
+        }
+        if (index !== row.length - 1) {
+          square.classList.add('right-border');
+        }
         square.setAttribute('data-id', `${rowIndex}-${index}`);
         square.innerText = item;
         board.append(square);
